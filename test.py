887f68ac7eea4856e20dffdc691b389209bc6139
namvinhkhang <83082969+namvinhkhang@users.noreply.github.com>
2023-10-10 12:11:47 -0400

    Update test.py

--- test.py
+++ test.py
@@ -7,17 +7,26 @@ class Link:
         self.exist = False
     
 if __name__ == "__main__":
-    probability = float(input("Enter probability: "))
+    
+    probability = float(input("Enter probability in percentage: "))
+    x10 = len(str(probability).rsplit(".",maxsplit=1)[-1])
+    probability *= 10 ** x10
+    range_prob = 100 * (10 ** x10)
     decoherence = float(input("Enter decoherence: "))
+    num_of_simulation = int(input("Enter number of simulations in interger: "))
     final_lambda = []
-    time = []
-    for i in range (10):
-        t, time_p1, time_p2 = 0, [], []
+    time, time_p1, time_p2 = [], [], []
+    
+    #DOING SIMULATION
+    for i in range (num_of_simulation):
+        t = 0
+        
+        #PHASE 1
         l1 = Link()
         l2 = Link()
         while not (l1.exist and l2.exist):
-            r1 = random.randint(0,100)
-            r2 = random.randint(0,100)
+            r1 = random.randint(0, range_prob)
+            r2 = random.randint(0, range_prob)
             if l1.exist:
                 l1.val *= decoherence
             elif r1 <= probability:
@@ -29,11 +38,13 @@ if __name__ == "__main__":
             t += 1
         time_p1.append(t)
         
+        
+        #PHASE 2
         l3 = Link()
         l4 = Link()
         while not (l3.exist or l4.exist):
-            r3 = random.randint(0,100)
-            r4 = random.randint(0,100)
+            r3 = random.randint(0, range_prob)
+            r4 = random.randint(0, range_prob)
             if l3.exist:
                 l3.val *= decoherence
             elif r3 <= probability:
@@ -47,13 +58,15 @@ if __name__ == "__main__":
             t += 1
         time_p2.append(t - time_p1[-1])
         
+        #CALCULATING THE FINAL LAMBDA
         if l3.exist:
             final_lambda.append(l1.val * l2.val * l3.val)
         else:
             final_lambda.append(l1.val * l2.val * l4.val)
         time.append(t)
+        
+    #PLOTING
     fig_1 = pyplot.figure(1, figsize=(20,5))
     chart_1 = fig_1.add_subplot(121)
-    
     chart_1.plot(time, final_lambda, 'o')
     pyplot.show()
