4a7a111c810bc73ed7756e553699e99a2e2178b5
namvinhkhang <83082969+namvinhkhang@users.noreply.github.com>
2023-10-10 11:55:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,29 +2,32 @@ from matplotlib import pyplot
 import random
 
 class Link:
-    def __init__(self) -> None:
-        self.val = 1
+    def __init__(self, val_lambda = 1) -> None:
+        self.val = val_lambda
         self.exist = False
     
 if __name__ == "__main__":
+    probability = float(input("Enter probability: "))
+    decoherence = float(input("Enter decoherence: "))
     final_lambda = []
     time = []
-    for i in range (1000):
-        t = 0
+    for i in range (10):
+        t, time_p1, time_p2 = 0, [], []
         l1 = Link()
         l2 = Link()
-        while not l1.exist and l2.exist:
+        while not (l1.exist and l2.exist):
             r1 = random.randint(0,100)
             r2 = random.randint(0,100)
             if l1.exist:
-                l1.val *= 0.98
-            elif r1 >= 50:
+                l1.val *= decoherence
+            elif r1 <= probability:
                 l1.exist = True
             if l2.exist:
-                l2.val *= 0.98
-            elif r2 >= 50:
+                l2.val *= decoherence
+            elif r2 <= probability:
                 l2.exist = True
             t += 1
+        time_p1.append(t)
         
         l3 = Link()
         l4 = Link()
@@ -32,21 +35,25 @@ if __name__ == "__main__":
             r3 = random.randint(0,100)
             r4 = random.randint(0,100)
             if l3.exist:
-                l3.val *= 0.98
-            elif r3 >= 50:
+                l3.val *= decoherence
+            elif r3 <= probability:
                 l3.exist = True
             if l4.exist:
-                l4.val *= 0.98
-            elif r4 >= 50:
+                l4.val *= decoherence
+            elif r4 <= probability:
                 l4.exist = True
-            l1.val *= 0.98
-            l2.val *= 0.98
+            l1.val *= decoherence
+            l2.val *= decoherence
             t += 1
+        time_p2.append(t - time_p1[-1])
         
         if l3.exist:
             final_lambda.append(l1.val * l2.val * l3.val)
         else:
             final_lambda.append(l1.val * l2.val * l4.val)
         time.append(t)
-    pyplot.scatter(time, final_lambda)
+    fig_1 = pyplot.figure(1, figsize=(20,5))
+    chart_1 = fig_1.add_subplot(121)
+    
+    chart_1.plot(time, final_lambda, 'o')
     pyplot.show()
